0fc814479ce5adf0c2e7975c5c4cbe0b9db36ead
unknown <socgend18@iiht.tech>
2020-07-08 22:43:01 +0530

    1st commit validation left

---
+++ assignment1/Controllers/productController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using assignment1.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace assignment1.Controllers
+{
+    public class productController : Controller
+    {
+        public static List<Product> list = new List<Product>() {
+            new Product{pid=1 ,pname="Moto one fusion plus",price=17499,stock=40},
+            new Product{pid=2 ,pname="Redmi 8",price=7499,stock=40}
+
+        };
+        public IActionResult Index()
+        {
+            return View(list);
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Product p)
+        {
+            list.Add(p);
+            return RedirectToAction("Index");
+
+        }
+        public IActionResult Validate(Product prod)
+        {
+            if (ModelState.IsValid)
+            {
+                list.Add(prod);
+                return RedirectToAction("Index");
+            }
+            else
+                return RedirectToAction("Create");
+        }
+    }
+}
---
+++ assignment1/Models/Product.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Diagnostics.HealthChecks;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace assignment1.Models
+{
+
+    public class Product
+    {
+        [Required(ErrorMessage = "Product ID is required")]
+       
+        public int pid { get; set; }
+        [Required(ErrorMessage = "Product Name is required")]
+        
+        public string pname { get; set; }
+        [Required(ErrorMessage = "Product Price is required")]   
+        public double price { get; set; }
+        [Required(ErrorMessage = "Product Stock is required")]
+        public int stock { get; set; }
+
+    }
+}
